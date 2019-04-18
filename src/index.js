8767e1b2926a399a590a84773fdf57176ca22641
Hope Hutman <hhutman@telefonprojekt.com>
2019-04-18 14:23:10 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Tweet (){
+  return (
+    <div className="tweet">
+    <Avatar/>
+    <div className="content">
+    <NameWithHandle/><Time/>
+    <Message/>
+    <div className="buttons">
+    <ReplyButton />
+    <RetweetButton />
+    <LikeButton />
+    <MoreOptionsButton />
+    </div>
+    </div>
+    </div>
+  );
+}
+
+function Avatar (){
+  return (
+    <img src="https://www.gravatar.com/avatar"
+    className="avatar"
+    alt="avatar"
+    />
+
+  )
+}
+
+function Message() {
+  return (
+
+    <div className="message">
+    This is less than 140 characters.
+    </div>
+
+  )
+}
+
+function NameWithHandle() {
+  return(
+    <span className="name-with-handle">
+    <span className="name">
+    Your name
+    </span>
+    <span className="handle">
+    @yourhandle
+    </span>
+    </span>
+  )
+}
+
+const Time = () =>
+<span className="time">
+  3hr ago
+  </span>;
+
+
+
+
+  const ReplyButton = () =>
+  <i className='fas fa-reply'></i>
+
+  const RetweetButton = () =>
+  <i className='fas fa-retweet'></i>
+
+  const MoreOptionsButton = () =>
+  <i className='fas fa-ellipsis-h'></i>
+
+  const LikeButton = () =>
+  <i className='fas fa-heart'></i>
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<Tweet />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
